7b357c37bb1216f3e0b983696f1a2bb84b453270
Srini Vasudevan <srini@gofundraise.com>
2024-01-21 22:45:25 +1100

    update handler

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 exports.handler = async function (event, context) {
+    var gameId = "cf3c538f";
+    var body = JSON.parse(event.body);
+    if (body.pid && body.vid) {
+        // retrieve the gameId from db
+    }
     const lib = require('./lib/getGameResults.js')
-    return await lib.getGameResults("cf3c538f");
+    return await lib.getGameResults(gameId);
 }
