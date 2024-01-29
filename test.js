c37403c41d73a3e64233a92db5b6f06fd67380fd
Srini Vasudevan <srini@gofundraise.com>
2024-01-29 14:41:42 +1100

    add the not out players and their totals to an array

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const lib = require('./index.js');
+const getCurrentBattersLib = require('./lib/getCurrentBatters.js')
 
 const start = async function() {
     var res = await lib.retrieveGameResults({vid: "xxx", pid: "yyy"});
@@ -10,5 +11,13 @@ const testUpdateGameId = async function(gameId) {
     console.log(res)
 }
 
+
+const testGetCurrentBatters = function(data) {
+    var data = require('./data/finished_game_1.json')
+    var res = getCurrentBattersLib.getCurrentBatters(data)
+    console.log(res)
+}
+
 //start()
-testUpdateGameId("545af13f")
+//testUpdateGameId("545af13f")
+testGetCurrentBatters()
