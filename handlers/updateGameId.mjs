5bc06fe0495d7b3e6809c6e456382804374ba9ad
Srini Vasudevan <srini@gofundraise.com>
2024-02-03 17:37:57 +1100

    update game changes

--- handlers/updateGameId.mjs
+++ handlers/updateGameId.mjs
@@ -1,7 +1,7 @@
+import settingsManager from '../lib/settingsManager.mjs'
 export default async function (event, context) {
   console.log("=== Executing Function Handler: updateGameId ===")
   if (event && event.gameId && event.pid && event.vid) {
-      var settingsManager = require('../lib/settingsManager.mjs').settingsManager;
       var res = await settingsManager.updateGameId(event.vid, event.pid, event.gameId)
       console.log(res);
   }
